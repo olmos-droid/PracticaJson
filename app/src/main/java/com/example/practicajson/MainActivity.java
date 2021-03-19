7a796f77ccef297051368cf4c1550e0495b63444
Josep Mª Olmos <56126978+olmos-droid@users.noreply.github.com>
2021-03-19 22:58:55 +0100

    Ejercicio de recycle con un json local

---
+++ app/src/main/java/com/example/practicajson/MainActivity.java
@@ -0,0 +1,91 @@
+package com.example.practicajson;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    public static final String TAG = "TAG";
+    RecyclerView recyclerView;
+    List<String> peakNames = new ArrayList<>();
+    List<String> peakHeight = new ArrayList<>();
+    List<String> peakImages = new ArrayList<>();
+    List<String> peakCountry = new ArrayList<>();
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Hook
+
+        recyclerView = findViewById(R.id.recyclerView);
+
+        //Layout
+
+        recyclerView.setLayoutManager(new
+                LinearLayoutManager(getApplicationContext()));
+
+        //Instanciar MyAdapter i assignar-lo al RecyclerView
+        MyAdapter myAdapter = new MyAdapter(MainActivity.this, peakNames, peakHeight,
+                peakImages, peakCountry);
+        recyclerView.setAdapter(myAdapter);
+
+        try {
+            //Getting JSONObject from file
+            JSONObject obj = new JSONObject(loadJSONObjectefromAssets());
+            //obtenir el JSONArray aqui
+            JSONArray peakArray = obj.getJSONArray("peaks");
+            for (int i = 0; i< peakArray.length(); i++) {
+                JSONObject peakDetail = peakArray.getJSONObject(i);
+                //afegir les dades a cada un dels ArrayList
+                peakNames.add(peakDetail.getString("name"));
+                peakHeight.add(peakDetail.getString("height"));
+                peakImages.add(peakDetail.getString("url"));
+                peakCountry.add(peakDetail.getString("country"));
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+
+
+    }
+    private String loadJSONObjectefromAssets() {
+
+        String json = null;
+        try {
+            InputStream input = getAssets().open("peaks.json");
+            //Returns an estimate of the number of bytes that can be read
+            int size = input.available();
+            Log.d(TAG, "loadJSONObjectefromAssets: size  json : "+size);
+            byte[] buffer = new byte[size];
+            //Reads some number of bytes from the input stream
+            //and stores them into the buffer array.
+            input.read(buffer);
+            input.close();
+            json = new String(buffer, "UTF-8"); //accpet all char types
+        } catch (IOException e) {
+            e.printStackTrace();
+
+            return null;
+        }
+        Log.d(TAG, "loadJSONObjectefromAssets: json loaded");
+        return json;
+    }
+
+}
---
+++ app/src/main/java/com/example/practicajson/MyAdapter.java
@@ -0,0 +1,80 @@
+package com.example.practicajson;
+
+import android.content.Context;
+import android.net.Uri;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
+    public static final String TAG = "TAG";
+    private Context mContext;
+    private List<String> peakNames;
+    private List<String> peakHeight;
+    private List<String> peakUrl;
+    private List<String> peakCountry;
+
+    public MyAdapter(Context context, List<String> peakNames, List<String> peakHeight, List<String> peakUrl, List<String> peakCountry) {
+        this.mContext = context;
+        this.peakNames = peakNames;
+        this.peakHeight = peakHeight;
+        this.peakUrl = peakUrl;
+        this.peakCountry = peakCountry;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater = LayoutInflater.from(mContext);
+        View view = layoutInflater.inflate(R.layout.peak_layout, parent, false);
+        return new MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+
+
+        Picasso.get().load(peakUrl.get(position)).into(holder.imageUrl);
+
+
+        Log.d(TAG, "onBindViewHolder: Url image " + position + " " + peakUrl.get(position));
+
+
+        holder.textName.setText(peakNames.get(position));
+        holder.textHeight.setText(peakHeight.get(position));
+        holder.textCountry.setText(peakCountry.get(position));
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return peakNames.size();
+
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        TextView textName;
+        TextView textHeight;
+        ImageView imageUrl;
+        TextView textCountry;
+
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            textName = itemView.findViewById(R.id.textName);
+            textHeight = itemView.findViewById(R.id.textHeight);
+            imageUrl = itemView.findViewById(R.id.imageUrl);
+            textCountry = itemView.findViewById(R.id.textCountry);
+        }
+    }
+}
